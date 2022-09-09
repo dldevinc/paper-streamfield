23984a57c699a09ff3dfddc109a377605b02400b
pix666 <pix666@ya.ru>
2022-09-09 17:02:34 +0400

    add helpers.py

--- streamfield/templatetags/streamfield.py
+++ streamfield/templatetags/streamfield.py
@@ -1,13 +1,7 @@
-import json
-from typing import Dict
-
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
-from django.core.handlers.wsgi import WSGIRequest
 from django.template.library import Library
 from django.utils.safestring import mark_safe
 
-from .. import blocks
-from ..logging import logger
+from .. import helpers
 
 try:
     import jinja2
@@ -18,39 +12,10 @@ except ImportError:
 register = Library()
 
 
-def _render_stream(stream: str, context: Dict, request: WSGIRequest) -> str:
-    if isinstance(stream, str):
-        stream = json.loads(stream)
-
-    if not all(blocks.is_valid(record) for record in stream):
-        logger.warning("Invalid stream value")
-        return ""
-
-    output = []
-    for record in stream:
-        try:
-            block = blocks.from_dict(record)
-        except (LookupError, ObjectDoesNotExist, MultipleObjectsReturned):
-            logger.warning("Invalid block: %r", record)
-            continue
-        else:
-            output.append(
-                blocks.render(
-                    block,
-                    extra_context={
-                        "parent_context": context
-                    },
-                    request=request
-                )
-            )
-
-    return "\n".join(output)
-
-
 @register.simple_tag(name="render_stream", takes_context=True)
 def do_render_stream(context, stream: str):
     request = context.get("request", None)
-    output = _render_stream(stream, context.flatten(), request=request)
+    output = helpers.render_stream(stream, context.flatten(), request=request)
     return mark_safe(output)
 
 
@@ -79,7 +44,7 @@ if jinja2 is not None:
         @staticmethod
         def _render_stream(stream: str, context):
             request = context.get("request", None)
-            return _render_stream(stream, context, request=request)
+            return helpers.render_stream(stream, context, request=request)
 
 
     # django-jinja support
